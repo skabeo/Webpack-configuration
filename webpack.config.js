b1e639ea28a46ba0bc9a84688a578444a449ff1c
skabeo <spencerokyere95@gmail.com>
2023-03-28 11:12:54 +0100

    Setup web pack configuration

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'webpack App',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ]
+}
